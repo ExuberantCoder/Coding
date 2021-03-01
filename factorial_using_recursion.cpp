6adf77330a25f7dc69305217298a1bd633d70586
satyam-gupta-github <64614610+satyam-gupta-github@users.noreply.github.com>
2021-03-01 08:41:15 +0530

    Create factorial_using_recursion.cpp

---
+++ factorial_using_recursion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int fact(int n)
+{
+    int ans=0;
+    if (n==0)
+    {
+        return 1;
+    }
+    ans=n*fact(n-1);
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
