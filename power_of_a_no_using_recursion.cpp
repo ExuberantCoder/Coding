26ae92e80fea9ab61e254afb874ce3f059fad03e
satyam-gupta-github <64614610+satyam-gupta-github@users.noreply.github.com>
2021-03-01 09:21:21 +0530

    Create power_of_a_no_using_recursion.cpp

---
+++ power_of_a_no_using_recursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int power(int n,int p)
+{
+    if(p==0)
+    {
+        return 1;
+    }
+    int a=n*power(n,p-1);
+    return a;
+}
+int main()
+{
+    int n,p;
+    cin>>n>>p;
+    cout<<power(n,p);
+    return 0;
+}
